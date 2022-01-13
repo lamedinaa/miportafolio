6dc5a02082ea15dcbf62705b23f39dd12d0d14ad
Alejandro <lamedinaa@gmail.com>
2022-01-12 22:02:40 -0500

    update productivo

--- miportafolio/settings.py
+++ miportafolio/settings.py
@@ -109,6 +109,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR.parent,'static'),)
